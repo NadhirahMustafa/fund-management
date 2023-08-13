ba28cf674527b6b3cf703e8efe019aae490c218d
Nadhirah Mustafa <nadhirahmustafa5@gmail.com>
2023-08-13 22:01:19 +0800

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <LandingPage pageTitle="Page title here"/>  
+  <FundDetails />
+  <HistoryDetails />
+</template>
+
+<script>
+import LandingPage from './components/LandingPage.vue'
+import FundDetails from './components/FundDetails.vue'
+// import HistoryDetails from './components/HistoryDetails.vue'
+
+export default {
+  name: 'App',
+  components: {
+    LandingPage,
+    FundDetails
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
